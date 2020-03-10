3283272ccea48ab2a4e4ad7b43f88addfe58d9c2
celilkurt <celilkurt82@gmail.com>
2020-03-10 21:12:20 +0300

    first

--- src/algorithms/Algorithms.java
+++ src/algorithms/Algorithms.java
@@ -13,12 +13,21 @@ public class Algorithms {
     
     public static void main(String[] args) {
         
-        int[] arr = {46,23,46,2345,1,45,46,89690,46,45,46,2344,46,123,40,45};
+        /*int[] arr = {46,23,46,2345,1,45,46,89690,46,45,46,2344,46,123,40,45};
         MergeSort.mergeSort(arr, 0, arr.length);
-        
         System.out.println(Arrays.toString(arr));
         
-        //System.out.println(Fibonacci.findNthFibonacci(18975));
+        System.out.println(Fibonacci.findNthFibonacci(18975));
+        
+        //1902'ye kadarki sayıların asal olup olmadığını ve asal çarpanlarını yazdırır.
+        for(int i = 2; i < 26540; i++){
+            System.out.println(i + " " + PrimeNumbers.isPrime(i));
+            System.out.println(PrimeNumbers.findPrimeFactors(i).toString());
+        
+        }
+        //Gönderilen değere kadar olan asal sayıları bulur.
+        System.out.println(PrimeNumbers.sieveOfEratos(291).toString());
+        */
         
         
         
---
+++ src/algorithms/PrimeNumbers.java
@@ -0,0 +1,91 @@
+
+
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class PrimeNumbers {
+    
+    public static ArrayList<Integer> findPrimeFactors(int n){
+        
+        ArrayList<Integer> factorList = new ArrayList<>();
+        
+        if(n < 2)
+            return new ArrayList<>();
+        
+        int sqrt = (int)Math.sqrt(n)+1;
+        for(int i = 2; i < sqrt && n > 1; i++){
+            
+            if(isPrime(i)){
+                
+                while(n%i == 0){
+                    factorList.add(i);
+                    n = (int)n/i;
+                }
+            }
+            
+            
+        }
+        
+        if(n != 1)
+            factorList.add(n);
+        
+        return factorList;
+    }
+    
+    public static boolean isPrime(int n){
+        
+        if(n == 2)
+            return true;
+        else if(n < 2)
+            return false;
+        else if(n%2 == 0)
+            return false;
+        
+        
+        int sqrt = (int)Math.sqrt(n)+1;
+        for(int i = 3; i < sqrt; i += 2)
+            if(n%i == 0)
+                return false;
+        
+        return true;
+    }
+    
+    /*
+        *In mathematics, the Sieve of Eratosthenes is a simple and ingenious
+        *ancient algorithm for finding all prime numbers up to any given limit.
+    
+    */
+    public static ArrayList<Integer> sieveOfEratos(int n){
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        boolean[] numArr = new boolean[n+1];
+        Arrays.fill(numArr, true);
+        numArr[0] = false;
+        numArr[1] = false;
+        
+        
+        for(int i = 2; i < n+1; i++){
+            
+            if(numArr[i]){
+                for(int j = 0; j < (int)(n)/i+1; j++)
+                    numArr[j*i] = false;
+                
+                list.add(i);
+            }
+                
+        }
+        
+        return list;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
