6b0ea0e93e47ddf82e257ae561d62c79f03aa8e9
celilkurt <celilkurt82@gmail.com>
2021-01-26 18:49:39 +0300

    hackerrank fp

---
+++ scala-algorithms/src/main/scala/hackerrank/fp/ValidBST.scala
@@ -0,0 +1,26 @@
+package hackerrank.fp
+
+import scala.io.StdIn.{readInt, readLine}
+
+// 3 2 1 5 4 6
+
+object ValidBST {
+
+  def main(args: Array[String]): Unit = {
+
+    val t = readInt
+    for(i <- 0 until t){
+
+      val numCount = readInt
+      val nums = for(num <- readLine.split(" ")) yield num.toInt
+
+
+    }
+
+  }
+
+
+
+
+
+}
